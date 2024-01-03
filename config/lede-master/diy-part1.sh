60f30199c3a116e454dded6d0ea6d33cbccaa43e
白勺啊的 <67247524+a2596519067@users.noreply.github.com>
2024-01-03 15:40:40 +0800

    Update diy-part1.sh

--- config/lede-master/diy-part1.sh
+++ config/lede-master/diy-part1.sh
@@ -9,6 +9,11 @@
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
+sed -i '2a ifconfig rai0 up\nifconfig ra0 up\nbrctl addif br-lan rai0\nbrctl addif br-lan ra0' package/base-files/files/etc/rc.local
+
+# 单独拉取软件包
+git clone -b Lienol-default-settings https://github.com/a2596519067/other package/default-settings
+
 # other
 # rm -rf package/lean/{samba4,luci-app-samba4,luci-app-ttyd}
 
